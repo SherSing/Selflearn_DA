cad4ad9f116f863d7aa19fcae9d4f866175396a7
Simar Pal Singh <107701000+SherSing@users.noreply.github.com>
2025-01-28 21:27:58 +0000

    Mean-Median-Mode_Data0

--- Task1.py
+++ Task1.py
@@ -1,6 +1,17 @@
 import numpy as np
 #Mean-Median-Mode
+import pandas as pd
+from scipy import stats
 
-array1=np.array([2,3,4,10])
-meanarray=np.mean(array1)
-print(meanarray)
+df = pd.read_csv('Raw Data/testdata.csv')
+print(df.head(5))
+
+meanLN_IC50=np.mean(df['LN_IC50'])
+print(meanLN_IC50)
+
+medianLN_IC50=np.median(df['LN_IC50'])
+print(medianLN_IC50)
+
+
+mode = stats.mode(df['LN_IC50'])
+print(mode.mode)
