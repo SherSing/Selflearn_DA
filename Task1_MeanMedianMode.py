0d587dd981bf96eec3c8a3da0cd711438ed7b5cc
Gurnoor Singh <gurnoor1990@gmail.com>
2025-06-04 21:49:47 +0200

    added jupyter notebook and modified Task1_MeanMedianMode.py

--- Task1_MeanMedianMode.py
+++ Task1_MeanMedianMode.py
@@ -3,15 +3,16 @@ import numpy as np
 import pandas as pd
 from scipy import stats
 
+def calculate_statistics(df, column):
+    mean_val = np.mean(df[column])
+    median_val = np.median(df[column])
+    mode_val = stats.mode(df[column], keepdims=True).mode[0]
+    return mean_val, median_val, mode_val
+
 df = pd.read_csv('Raw Data/testdata.csv')
 print(df.head(5))
 
-meanLN_IC50=np.mean(df['LN_IC50'])
+meanLN_IC50, medianLN_IC50, modeLN_IC50 = calculate_statistics(df, 'LN_IC50')
 print(meanLN_IC50)
-
-medianLN_IC50=np.median(df['LN_IC50'])
 print(medianLN_IC50)
-
-
-mode = stats.mode(df['LN_IC50'])
-print(mode.mode)
+print(modeLN_IC50)
